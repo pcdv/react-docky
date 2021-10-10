31a16894fff92d8c62098c076e004d3ed6278d9a
pcdv <>
2021-10-10 18:01:08 +0200

    Initial version  - can provide an initial state and a render function  - all views are split evenly  - react-split-pane is used to separate panes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Box, Dockable, View } from './dockable/Dockable'
+
+const state: Box = {
+  type: 'box',
+  orientation: 'vertical',
+  first: {
+    type: 'box',
+    orientation: 'horizontal',
+    first: {
+      type: 'tabs',
+      tabs: [{ type: 'view', id: 'red', label: 'Red', viewType: 'colored' }],
+    },
+    second: {
+      type: 'box',
+      orientation: 'vertical',
+      first: {
+        type: 'tabs',
+        tabs: [
+          { type: 'view', id: 'green', label: 'Green', viewType: 'colored' },
+        ],
+      },
+      second: {
+        type: 'tabs',
+        tabs: [
+          { type: 'view', id: 'orange', label: 'Orange', viewType: 'colored' },
+        ],
+      },
+    },
+  },
+  second: {
+    type: 'tabs',
+    tabs: [{ type: 'view', id: 'blue', label: 'Blue', viewType: 'colored' }],
+  },
+}
+
+function render(view: View) {
+  return (
+    <div className="fill" style={{ background: view.id, color: 'white' }}>
+      {view.label}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dockable state={state} render={render} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
