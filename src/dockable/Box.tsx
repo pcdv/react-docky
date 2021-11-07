a2e64aca88d051fd76213e9c487368d8e7645eba
pcdv <pcdv@local>
2021-11-07 09:44:17 +0100

    Pass rank explicitly to view container

--- src/dockable/Box.tsx
+++ src/dockable/Box.tsx
@@ -16,7 +16,7 @@ export const Box = ({ box, render, onChange }: BoxProps) => {
   const horizontal = box.orientation === 'horizontal'
   if (box.one && box.two)
     return (
-      <div key={box.id} className="fill" id={`box-${box.id}`}>
+      <div key={box.id} className="" id={`box-${box.id}`}>
         <DropZone box={box} action="o1" position={horizontal ? 'top' : 'left'} />
         <DropZone box={box} action="o2" position={horizontal ? 'bottom' : 'right'} />
         <SplitPane
@@ -24,20 +24,21 @@ export const Box = ({ box, render, onChange }: BoxProps) => {
           className="box"
           defaultSize="50%"
         >
-          {renderAny(box.one, render, onChange, box)}
-          {renderAny(box.two, render, onChange, box)}
+          {renderAny(1, box.one, render, onChange, box)}
+          {renderAny(2, box.two, render, onChange, box)}
         </SplitPane>
       </div>
     )
   return (
     <div key={box.id} className={`box ${box.orientation}`}>
-      {!!box.one && renderAny(box.one, render, onChange, box)}
-      {!!box.two && renderAny(box.two, render, onChange, box)}
+      {!!box.one && renderAny(1, box.one, render, onChange, box)}
+      {!!box.two && renderAny(2, box.two, render, onChange, box)}
     </div>
   )
 }
 
 export function renderAny(
+  rank: 1 | 2,
   item: IBox | ITabs ,
   render: ViewRenderer,
   onChange: (action: BoxAction | ViewAction) => void,
@@ -45,12 +46,13 @@ export function renderAny(
 ): React.ReactNode {
   switch (item.type) {
     case 'box':
-      return <Box box={item} render={render} onChange={onChange} />
+      return <Box key={item.id} box={item} render={render} onChange={onChange} />
     case 'tabs':
       return (
         <ViewContainer
           key={item.id}
           tabs={item}
+          rank={rank}
           render={render}
           onChange={onChange}
           parent={parent}
